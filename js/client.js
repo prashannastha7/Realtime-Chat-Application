2b403bc3a6f6eadb8626625570f73a8ced8aaf57
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-02-22 11:33:02 +0545

    Backend

--- js/client.js
+++ js/client.js
@@ -32,3 +32,10 @@ socket.on('user-joined',name =>{
 
 })
 
+socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, 'left');
+})
+
+socket.on('left', name =>{
+    append(`${name} left the chat`, 'right');
+})
